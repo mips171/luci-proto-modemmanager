57028e0ed50d26735804225094a2ac105781f826
Nicholas Smith <mips171@icloud.com>
2018-10-30 11:29:25 +1000

    Added support for speciying IP connection type.
    Signed-off-by: Nicholas Smith <mips171@icloud.com>

--- luci-proto-modemmanager/luasrc/model/cbi/admin_network/proto_modemmanager.lua
+++ luci-proto-modemmanager/luasrc/model/cbi/admin_network/proto_modemmanager.lua
@@ -11,10 +11,10 @@ local auth, ipv6
 device = section:taboption("general", Value, "device", translate("Modem device"))
 device.rmempty = false
 
---local device_suggestions = nixio.fs.glob("/sys/devices/platform/1e1c0000.xhci/usb2/2-*")
--- Supports only one modem that has already been registered by MM.  Ensures the modem is usable.
-local device_suggestions = io.popen("mmcli -m 0 | grep 'device: ' | grep -Eo '/sys/devices/.*' | tr -d \"'\"", "r")
+--local handle = nixio.fs.glob("/sys/devices/platform/1e1c0000.xhci/usb2/2-*")
 --local handle = io.popen("realpath /sys/class/net/wwan0/device", "r")
+-- Supports only one modem that has already been registered by MM.  Ensures the modem is usable.
+local handle = io.popen("mmcli -m 0 | grep 'device: ' | grep -Eo '/sys/devices/.*' | tr -d \"'\"", "r")
 local device_suggestions = handle:read("*l")
 handle:close()
 
@@ -30,24 +30,22 @@ apn:value("internet", "Telstra: internet")
 apn:value("telstra.internet", "Telstra: telstra.internet")
 apn:value("connect", "Optus: connect")
 
-
 pincode = section:taboption("general", Value, "pincode", translate("PIN"))
 
-
 username = section:taboption("general", Value, "username", translate("PAP/CHAP username"))
-
 
 password = section:taboption("general", Value, "password", translate("PAP/CHAP password"))
 password.password = true
 
-auth = section:taboption("general", Value, "auth", translate("Authentication Type"))
+auth = section:taboption("general", Value, "auth", translate("Authentication type"))
 auth:value("", translate("-- Please choose --"))
 auth:value("both", "PAP/CHAP (both)")
 auth:value("pap", "PAP")
 auth:value("chap", "CHAP")
 auth:value("none", "NONE")
 
-if luci.model.network:has_ipv6() then
-    ipv6 = section:taboption("advanced", Flag, "ipv6", translate("Enable IPv6 negotiation"))
-    ipv6.default = ipv6.disabled
-end
+iptype = section:taboption("general", Value, "iptype", translate("IP connection type"))
+iptype:value("", translate("-- Please choose --"))
+iptype:value("ipv4", "IPv4 only")
+iptype:value("ipv6", "IPv6 only")
+iptype:value("ipv4v6", "IPv4/IPv6 (both - defaults to IPv4)")
