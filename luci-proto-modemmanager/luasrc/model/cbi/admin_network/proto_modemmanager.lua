3a3a13a1d8361c17608437acaf04ecb2dafe2e29
Nicholas Smith <mips171@icloud.com>
2018-10-30 11:02:38 +1000

    Added support for specifying IP connection type

--- luci-proto-modemmanager/luasrc/model/cbi/admin_network/proto_modemmanager.lua
+++ luci-proto-modemmanager/luasrc/model/cbi/admin_network/proto_modemmanager.lua
@@ -1,5 +1,6 @@
 -- Copyright 2018 Nicholas Smith <mips171@icloud.com>
--- Licensed to the public under the Apache License 2.0.
+-- Adapted from luci-proto-qmi
+-- Licensed to the public under the GNU GPL v2.0.
 
 local map, section, net = ...
 
@@ -10,12 +11,10 @@ local auth, ipv6
 device = section:taboption("general", Value, "device", translate("Modem device"))
 device.rmempty = false
 
-
--- Supports only one modem that has already been registered by MM.  Ensures the modem is usable.
-local handle = io.popen("mmcli -m 0 | grep 'device: ' | grep -Eo '/sys/devices/.*' | tr -d \"'\"", "r")
--- Here are a couple of other ways to do this and possibly support more devices.  Note: realpath should be added to BusyBox.
 --local handle = nixio.fs.glob("/sys/devices/platform/1e1c0000.xhci/usb2/2-*")
 --local handle = io.popen("realpath /sys/class/net/wwan0/device", "r")
+-- Supports only one modem that has already been registered by MM.  Ensures the modem is usable.
+local handle = io.popen("mmcli -m 0 | grep 'device: ' | grep -Eo '/sys/devices/.*' | tr -d \"'\"", "r")
 local device_suggestions = handle:read("*l")
 handle:close()
 
@@ -31,12 +30,9 @@ apn:value("internet", "Telstra: internet")
 apn:value("telstra.internet", "Telstra: telstra.internet")
 apn:value("connect", "Optus: connect")
 
-
 pincode = section:taboption("general", Value, "pincode", translate("PIN"))
 
-
 username = section:taboption("general", Value, "username", translate("PAP/CHAP username"))
-
 
 password = section:taboption("general", Value, "password", translate("PAP/CHAP password"))
 password.password = true
@@ -48,7 +44,8 @@ auth:value("pap", "PAP")
 auth:value("chap", "CHAP")
 auth:value("none", "NONE")
 
-if luci.model.network:has_ipv6() then
-    ipv6 = section:taboption("advanced", Flag, "ipv6", translate("Enable IPv6 negotiation"))
-    ipv6.default = ipv6.disabled
-end
+iptype = section:taboption("general", Value, "iptype", translate("IP Connection Type"))
+iptype:value("", translate("-- Please choose --"))
+iptype:value("ipv4", "IPv4 only")
+iptype:value("ipv6", "IPv6 only")
+iptype:value("ipv4v6", "IPv4/IPv6 (both - defaults to IPv4)")
